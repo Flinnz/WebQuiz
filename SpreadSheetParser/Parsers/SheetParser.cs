721518e4961b4824411668af1a3c96ab9314cb96
Dmitry Shchapin <diman199806@gmail.com>
2019-03-30 16:06:19 +0500

    google spreadsheet question parser

---
+++ SpreadSheetParser/Parsers/SheetParser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Util.Store;
+
+namespace SpreadSheetParser.Parsers
+{
+    public class SheetParser
+    {
+        private static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        private static string ApplicationName = "WebQuiz";
+
+        private readonly UserCredential credential;
+        private readonly string credentialPath = $"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}/token";
+        private readonly SheetsService service;
+        private static readonly string sheetId = "1FgbLOKoa1FuXnyiDLsweLoAtU60u3i0MlnMLJRCnE38";
+
+        public SheetParser()
+        {
+            
+            using (var stream = new FileStream($"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}/credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credentialPath, true)).Result;
+            }
+
+            service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+
+        public IList<IList<object>> GetValues(string range)
+        {
+            var request = service.Spreadsheets.Values.Get(sheetId, range);
+            var response = request.Execute();
+            var values = response.Values;
+            return values;
+        }
+
+    }
+}
---
+++ SpreadSheetParser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SpreadSheetParser.Parsers;
+
+namespace SpreadSheetParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var sheetParser = new SheetParser();
+
+            sheetParser.GetValues("B2:C");
+        }
+    }
+}
